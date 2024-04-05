8a6460a3be6dac7297a13acd5aa8136057003d22
NadiasCodes <De.nadia@outlook.com>
2024-04-05 12:56:30 +0200

    learned about updater function and changed my app based on that

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,14 +4,14 @@ const Counter = () => {
   const [count, setCount] = useState(0);
 
   const incrementButton = () => {
-    setCount(count + 1);
+    setCount(c => c + 1);
   };
   const decrementButton = () => {
-    setCount(count - 1);
+    setCount(c => c - 1);
   };
 
   const resetButton = () => {
-    setCount(0);
+    setCount(c => c = 0);
   };
   return (
     <div>
@@ -25,3 +25,10 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// If we want to increase the number when we click the button, by adding one more line of the existing function. It won't work
+// That is because we use the current state to calculate the next but set functions do not trigger an update
+// Next state becomes the current state. In the case that we need multiple state updates, we need to use updater function
+// We either do it by using prevCount or simply take the first letter from our variable
+// It's a good practice to write it like that even for a simple counter app
+// This way it takes the pending state not the current stage into the next stage
